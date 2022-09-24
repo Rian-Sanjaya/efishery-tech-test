67cbb8689e41e2c916a0162b0d4368a02cbfb83d
Rian Sanjaya <rian.sj@gmail.com>
2022-09-24 12:03:10 +0700

    feat(package): add stein client
    Add dependency stein-js-client, a client libraries to interact with the Stein core API
    
    Feat #0002

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,24 @@
+import {useState, useEffect} from "react";
+import { steinStore } from "../api/api-method";
+
 function App() {
+  const [comodities, setComodities] = useState(null);
+
+  useEffect(() => {
+    steinStore.read("list")
+      .then(res => {
+        const data = res.filter(item => item.uuid);
+        setComodities(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      Homepage
+      {comodities && comodities.length && comodities.map(comodity => (
+        <div key={comodity.uuid}>
+          { comodity.komoditas }
+        </div>
+      ))}
     </div>
   );
 }
